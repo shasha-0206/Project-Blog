244b484624e8c799b5b55b933d96c7a56680e799
Shashank <malkhedkarshashank@gmail.com>
2024-12-08 12:34:10 +0530

    updated Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -76,60 +76,60 @@ const Home = () => {
   const hasMorePosts = posts.length < totalPosts;
   return (
     <div className="container mb-3">
-    <style>
-      {`
-     
-        .post-card img {
-          transition: transform 0.3s ease-in-out;
-        }
-        .post-card:hover img {
-          transform: scale(1.01);
-        }
-      `}
-    </style>
-    <Toaster />
-    <h3 className="text-center mt-3">All Posts</h3>
+      <style>
+        {`
+       
+          .post-card img {
+            transition: transform 0.3s ease-in-out;
+          }
+          .post-card:hover img {
+            transform: scale(1.01);
+          }
+        `}
+      </style>
+      <Toaster />
+      <h3 className="text-center mt-3">All Posts</h3>
 
-    <div className="row row-cols-lg-3 row-cols-md-2 row-cols-1 g-3" >
-      {posts.length > 0 ? (
-        posts.map((post, index) => (
-          <div key={post._id} className="card col post-card" >
-            <img
-              src={`data:image/jpeg;base64,${post.image}`}
-              className="card-img-top"
-              alt="post_image"
-              style={{ height: '300px', cursor: 'pointer', objectFit: 'cover' }}
-              onClick={() => handlePostClick(post._id)}
-            />
-            <div className="card-body" style={{ cursor: 'pointer' }} onClick={() => handlePostClick(post._id)}>
-              <p className="card-text">
-                <b>{post.title}</b>
-                <br /> by {post.username}
-              </p>
+      <div className="row row-cols-lg-3 row-cols-md-2 row-cols-1 g-3" >
+        {posts.length > 0 ? (
+          posts.map((post, index) => (
+            <div key={post._id} className="card col post-card" >
+              <img
+                src={post.image.url}
+                className="card-img-top"
+                alt="post_image"
+                style={{ height: '300px', cursor: 'pointer', objectFit: 'cover' }}
+                onClick={() => handlePostClick(post._id)}
+              />
+              <div className="card-body" style={{ cursor: 'pointer' }} onClick={() => handlePostClick(post._id)}>
+                <p className="card-text">
+                  <b>{post.title}</b>
+                  <br /> by {post.username}
+                </p>
+              </div>
             </div>
+          ))
+        ) : (
+          <div className="loader-container d-flex justify-content-center align-items-center">
+            <div className="loader"></div>
           </div>
-        ))
-      ) : (
+        )}
+      </div>
+
+      {LoadMore ? (
         <div className="loader-container d-flex justify-content-center align-items-center">
           <div className="loader"></div>
         </div>
+      ) : (
+        hasMorePosts && (
+          <div className="d-flex justify-content-center mt-3">
+            <button onClick={handleLoadMore} className="btn btn-primary add-btn">
+              Load More
+            </button>
+          </div>
+        )
       )}
     </div>
-
-    {LoadMore ? (
-      <div className="loader-container d-flex justify-content-center align-items-center">
-        <div className="loader"></div>
-      </div>
-    ) : (
-      hasMorePosts && (
-        <div className="d-flex justify-content-center mt-3">
-          <button onClick={handleLoadMore} className="btn btn-primary add-btn">
-            Load More
-          </button>
-        </div>
-      )
-    )}
-  </div>
   );
 };
 
