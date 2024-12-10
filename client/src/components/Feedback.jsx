b9b8a1e4a18bd12be04a16864b10a16d63b208b6
shashank kumar <168854855+shasha-0206@users.noreply.github.com>
2024-12-10 11:49:22 +0530

    Update Feedback.jsx

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -28,25 +28,36 @@ const Feedback = () => {
     };
 
     return (
-        <div className="container mt-5 mb-5">
+       <div className="container mt-5 mb-5">
             <Toaster />
             <h2>Feedback Form</h2>
             <form ref={form} onSubmit={sendEmail}>
-                <div className="mb-3 mt-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" name="from_name" required />
+                <div className="row mb-3 mt-3">
+                    <div className="col-12">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" name="from_name" required />
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" name="form_email" required />
+                <div className="row mb-3">
+                    <div className="col-12">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" name="form_email" required />
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="message" className="form-label">Message</label>
-                    <textarea className="form-control" id="message" name="message" rows="4" required></textarea>
+                <div className="row mb-3">
+                    <div className="col-12">
+                        <label htmlFor="message" className="form-label">Message</label>
+                        <textarea className="form-control" id="message" name="message" rows="4" required></textarea>
+                    </div>
                 </div>
-                <button type="submit" className="btn btn-primary feedback-submit-btn">Submit</button>
+                <div className="row">
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-primary feedback-submit-btn">Submit</button>
+                    </div>
+                </div>
             </form>
         </div>
+
     );
 };
 
